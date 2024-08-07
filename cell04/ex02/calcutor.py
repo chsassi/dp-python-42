5509eec168a9d277e29a102f30d35817ff03dc6d
Christian Sassi <christian.sassii@gmail.com>
2024-08-07 18:02:12 +0200

    //

--- cell04/ex02/calcutor.py
+++ cell04/ex02/calcutor.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 def main():
-    x = int(input("Give me the first number: "))
-    y = int(input("Give me the second number: "))
-    print("Thank you!")
-    print(f"{x} + " f"{y} = " f"{x + y}")
-    print(f"{x} - " f"{y} = " f"{x - y}")
-    print(f"{x} / " f"{y} = " f"{x / y}")
-    print(f"{x} * " f"{y} = " f"{x * y}")
+    try:
+        x = int(input("Give me the first number: "))
+        y = int(input("Give me the second number: "))
+        print("Thank you!", "\n")
+        print(f"{x} + " f"{y} = " f"{x + y}")
+        print(f"{x} - " f"{y} = " f"{x - y}")
+        print(f"{x} * " f"{y} = " f"{x * y}")
+        print(f"{x} / " f"{y} = " f"{x / y}")
+    except:
+        print("Enter a valid number")
 
 if __name__ == "__main__":
     main()
--- cell06/ex00/hello_all.py
+++ cell06/ex00/hello_all.py
@@ -2,8 +2,13 @@
 
 import sys
 
+class Greetings:
+    string : str
+    def hello():
+        print("Hello, everyone!")
+
 def main():
-    
+    Greetings.hello()
 
 if __name__ == "__main__":
     main()
